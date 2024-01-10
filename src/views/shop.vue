0248d7cc4bb1e237101ce8955cdbc7ce60218199
karta1020146 <karta1020146@gmail.com>
2024-01-10 13:29:59 +0800

    update2

--- src/views/shop.vue
+++ src/views/shop.vue
@@ -31,7 +31,7 @@
 <template>
     <div class="shop">
         <div class="tag">
-            Category : 
+            Category : 123123
             <button @click="showAll">
                 All
             </button>
