49f1858af6ae1d90b8398f52581f6646ee866f02
karta1020146 <karta1020146@gmail.com>
2024-01-10 22:01:38 +0800

    updatecomp

--- src/views/components.vue
+++ src/views/components.vue
@@ -1,16 +1,30 @@
 <script setup>
 import {defineAsyncComponent} from 'vue'
 import food from "../components/food.vue";
-
 const slideComp = defineAsyncComponent(() => import("slide/slide"));
+import { useToogle } from "../composables/toogle";
+
+const {toogleFunction} = useToogle()
+
+const toogleValue = (true)
+
+const toogleChange =()=>{
+  toogleFunction(toogleValue)
+}
 
 </script>
 
 <template>
   <div class="components">
     <h1>This is an components page</h1>
-      <food />
-      <slideComp />
+      <div class="compWrap">
+        <transition-group>
+        
+        </transition-group>
+        <food v-if="toogleValue"/>
+        <slideComp v-else class="set"/>
+        <button @click="toogleChange">ch</button>
+      </div>
   </div>
 </template>
 
@@ -18,5 +32,12 @@ const slideComp = defineAsyncComponent(() => import("slide/slide"));
   h1{
     text-align: center;
     padding: 1.5% 0;
+  }
+  .set{
+    .control{
+      & .center{
+        width: 15%;
+      }
+    }
   }
 </style>
--- src/views/shop.vue
+++ src/views/shop.vue
@@ -31,7 +31,7 @@
 <template>
     <div class="shop">
         <div class="tag">
-            Category : aappaa
+            Category :
             <button @click="showAll">
                 All
             </button>
